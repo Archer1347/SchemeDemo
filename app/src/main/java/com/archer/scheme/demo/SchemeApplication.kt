5969a95adaa293e387f9d072d059f2d0d0630efd
林嘉强 <linjiaqiang@vv.cn>
2020-07-14 14:18:24 +0800

    修改注释

--- app/src/main/java/com/archer/scheme/demo/SchemeApplication.kt
+++ app/src/main/java/com/archer/scheme/demo/SchemeApplication.kt
@@ -4,16 +4,7 @@ import android.app.Application
 import com.archer.scheme.SchemeManager
 
 /**
- * Desc:
- * <p>
- * Date: 2020/7/14
- * Copyright: Copyright (c) 2010-2020
- * Company: @微微科技有限公司
- * Updater:
- * Update Time:
- * Update Comments:
- *
- * @author: linjiaqiang
+ * Created by ljq on 2020/7/14
  */
 class SchemeApplication : Application() {
 
--- app/src/main/java/com/archer/scheme/demo/SecondActivity.java
+++ app/src/main/java/com/archer/scheme/demo/SecondActivity.java
@@ -3,16 +3,7 @@ package com.archer.scheme.demo;
 import android.app.Activity;
 
 /**
- * Desc:
- * <p>
- * Date: 2020/7/10
- * Copyright: Copyright (c) 2010-2020
- * Company: @微微科技有限公司
- * Updater:
- * Update Time:
- * Update Comments:
- *
- * @author: linjiaqiang
+ * Created by ljq on 2020/7/14
  */
 public class SecondActivity extends Activity {
 
